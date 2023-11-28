2c13c57fff0f3579a7fc499dd26a76879d5eae39
daniela <gaofulou05@gmail.com>
2023-11-28 21:26:29 +0100

    versio5

--- my_mv.c
+++ my_mv.c
@@ -1,2 +1,31 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+void main(){
+	char nom[100], nou[100], buf[1];
+	int fd, desc; 
+
+	printf("Introdueix el nom de l'arxiu:\n"); 
+	scanf("%s", nom); 
+	printf("Nou nom/ nou lloc:\n"); 
+	scanf("%s", nou); 
+
+	//printf("%s, %s\n", nom, lloc); 
+	fd = open(nom, O_RDONLY);
+
+	if (fd<0){
+		printf("ERROR: L'arxiu no existeix"); 
+	}else{
+		desc = open(nou, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU); 
+		if (desc<0) {
+			printf("ERROR: No s'ha pogut crear"); 
+		}else{
+			while ((read(fd,buf,1)) >0){
+				write(desc,buf,1);
+			} 
+		close(desc);
+		}
+		unlink(nom); 
+	}
+}
