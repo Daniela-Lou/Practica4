6d9a3ea9be91218c11fecac27226dfb754ffcb99
daniela <gaofulou05@gmail.com>
2023-11-28 20:30:37 +0100

    versio4

--- my_head.c
+++ my_head.c
@@ -4,12 +4,12 @@
 
 void main(){
 	char arxiu[100], buf[1];
-	int linies, fd, num;
+	int linies, fd, num, i;
 	printf("Introdueix el nombre de l'arxiu:\n");
 	scanf("%s", arxiu);
 	printf("Nombre de línies a imprimir:\n");
 	scanf("%d", &linies);
-      	//printf("%s, %d\n", arxiu, linies);
+
 	fd = open(arxiu, O_RDONLY); 
 	if (fd<0) {
 		printf("ERROR: L'arxiu no existeix");
@@ -17,14 +17,18 @@ void main(){
 		if (linies<=0){
 			printf("Error: Línies igual a zero o negatives");
 		}else{
-			num=read(fd, buf, 1);
-			for (int i =0; i<linies; ){
-				if (buf[0]==10){
-					i++; 
-				}
+			read(fd, buf, 1);
+			for (i =0; i<linies; ){
+				printf("\n%d\n", linies);
+				if(buf[0]==10){
+					i++;
+				}	
 				write(1,buf,1);
-				num=read(fd,buf,1); 
-				
+				num = read(fd,buf,1);
+				//if (num=0){
+				//	break;
+				//}
+			       		
 			}
 		}
 	}
